e5d7fed4d8029a77e77b05ffdcb74eef0b25eced
Luuk Schukkink <luuk.schukkink@gmail.com>
2020-11-25 23:31:08 +0100

    bow enemy added

---
+++ Assets/scripts/arrow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public bool shoot = false;
+    public Vector3 speed;
+    public GameObject my_arrow;
+    public GameObject holder;
+    public bool left;
+    private float arrow_timer = 0;
+    private bool stop = false;
+    bool timerstart = false;
+    void OnEnable()
+    {
+        my_arrow.transform.position = holder.transform.position;
+        stop = false;
+        timerstart = false;
+        arrow_timer = 0f;
+    }
+    void FixedUpdate()
+    {
+        if (!stop)
+        {
+        if (left)
+        {
+            transform.position += speed;
+        }
+        if (!left)
+        {
+            transform.position -= speed;
+        }
+        }
+        if (timerstart)
+        {
+            if (arrow_timer > 2)
+            {
+                my_arrow.SetActive(false);
+            }
+            else 
+            {
+                arrow_timer += Time.deltaTime;
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap")
+        {
+            stop = true;
+            timerstart = true;
+        }
+    } 
+}
---
+++ Assets/scripts/shooting.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    [SerializeField] private LayerMask platformlayermask;
+    private BoxCollider2D boxCollider2d;
+    public Rigidbody2D rb2d;
+    public SpriteRenderer mySpriteRenderer;
+    public GameObject wapon;
+    private SpriteRenderer wapon_renderer;
+    public GameObject ammo;
+    private SpriteRenderer ammo_renderer;
+    private float shot_timer = 0f;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        wapon_renderer = wapon.GetComponent<SpriteRenderer>();
+        ammo_renderer = ammo.GetComponent<SpriteRenderer>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player_on_level_left())
+        {
+                mySpriteRenderer.flipX = false;
+                wapon_renderer.flipX = false;
+                ammo_renderer.flipX = false;
+                if (shot_timer > 3)
+                {
+                    ammo.SetActive(true);
+                    ammo.GetComponent<arrow>().left = false;
+                }
+                else
+                {
+                    shot_timer += Time.deltaTime;
+                }                
+        }
+        if (player_on_level_right())
+        {
+                mySpriteRenderer.flipX = true;
+                wapon_renderer.flipX = true;   
+                ammo_renderer.flipX = true; 
+                if (shot_timer > 3)
+                {
+                    ammo.SetActive(true);
+                    ammo.GetComponent<arrow>().left = true;
+                }
+                else
+                {
+                    shot_timer += Time.deltaTime;
+                }
+
+
+        }
+        if (!player_on_level_left())
+        {
+            if (!player_on_level_right())
+            {
+            shot_timer = 0f;
+            }
+        }
+    }
+    private float extra = 5f;
+    private bool player_on_level_left()
+    {   
+        RaycastHit2D raycastHit1 = Physics2D.Raycast(boxCollider2d.bounds.center ,
+                Vector2.left, boxCollider2d.bounds.extents.x + extra, platformlayermask);
+        return raycastHit1.collider != null; 
+    }
+    private bool player_on_level_right()
+    {   
+        RaycastHit2D raycastHit2 = Physics2D.Raycast(boxCollider2d.bounds.center ,
+                Vector2.right, boxCollider2d.bounds.extents.x + extra, platformlayermask);
+        return raycastHit2.collider != null; 
+    }
+}
